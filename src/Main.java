794e3170b6170614585c61cfe4ec85821f2bf25c
Андрей Васюхин <arikhelvi@gmail.com>
2022-11-25 12:50:07 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,29 @@ import java.util.Scanner; // импортируем класс
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Ведите размер массива:");
-        int n;
-        if(sc.hasNextInt()) {
-            n = sc.nextInt();
-        }else{
-            System.out.print("Вы ввели неккоректные данные. попытайтесь ещё раз позже.");
-            return;
+        int n=0, temp;
+        System.out.print("Ведите новый объект для массива:");
+        if(sc.hasNextInt()) {temp = sc.nextInt();
+        }else{System.out.print("Вы ввели неккоректные данные. попытайтесь ещё раз позже.");return;
         }
-        int[] mas = new int[n];
-        for(int i=0;i<n;i++){
-            System.out.print("Ведите новый объект для массива:");
-            if(sc.hasNextInt()) {mas[i] = sc.nextInt();}
-            else{System.out.print("Вы ввели неккоректные данные. попытайтесь ещё раз позже.");return;}
+        int s=0;int min=0, max=0;
+        min = temp;max = temp;
+        System.out.print("Введите новое число для заполнения массива");
+        System.out.print("\nили введите любой не-циферный символ чтобы прекратить:");
+        while(sc.hasNextInt()){
+            temp = sc.nextInt();
+            System.out.print("Введите новое число для заполнения массива");
+            System.out.print("\nили введите любой не-циферный символ чтобы прекратить:");
+            s+=temp;n++;
+            if(temp>max){max = temp;
+            }else if (temp<min){min = temp;
+            }
         }
-        int s=0;
-        for(int i=0;i<n;i++){
-            s+=mas[i];
-        }
-        int ans = s/n;
+        System.out.print("Сумма членов массива:");
+        System.out.print(s);
+        System.out.print("\nМинимум массива:");
+        System.out.print(min);
+        System.out.print("\nМаксимум массива:");
+        System.out.print(max);
     }
 }
